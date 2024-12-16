d20ae6adf8f5ecdc93f2b5170b873ac3ba46c5e9
Thomas Jefferson <TOMartilence>
2024-12-17 00:52:35 +0530

    o

--- src/Components/StudentCard.jsx
+++ src/Components/StudentCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function StudentCard({ student }) {
   return (
-    <div className="card">
+    <div className="card" style={{padding : "30px"}}>
       <h2>{student.name}</h2>
       <p><strong>Roll Number:</strong> {student.roll}</p>
       <p><strong>Year:</strong> {student.year}</p>
