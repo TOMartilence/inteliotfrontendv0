079a9ebeb44621ba5ff803c0892c3c82c9eeb065
NarenSUndar007 <narensundar18@gmail.com>
2025-01-05 17:24:23 +0530

    Made a fixed size for all project cards

--- src/Components/ProjectCards.jsx
+++ src/Components/ProjectCards.jsx
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import dummy from "../Images/you.jpeg";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Loader from "../Components/Loader"
+import Loader from "../Components/Loader";
+
 function ProjectCards(props) {
-  const [loader,setLoader] = useState("none");
+  const [loader, setLoader] = useState("none");
+
   const handleDelete = async (id) => {
     try {
       setLoader("");
       const response = await axios.post(`${process.env["REACT_APP_backendbaseurl"]}/api/delete/${id}`);
-      if(response.data.success){
-        alert(response.data.message)
-        setLoader("none")
+      if (response.data.success) {
+        alert(response.data.message);
+        setLoader("none");
       }
     } catch (error) {}
     console.log("Card deleted:", props.title);
@@ -19,24 +21,35 @@ function ProjectCards(props) {
 
   return (
     <div>
-      <Loader display={loader}/>
-      <div className="card" style={{ maxWidth: "22rem" }}>
-        <div className="view overlay">
+      <Loader display={loader} />
+      <div className="card" style={{ maxWidth: "22rem", display: "flex", flexDirection: "column", height: "400px" }}>
+        <div className="view overlay" style={{ flex: "1 1 auto", overflow: "hidden" }}>
           {props.imagelink ? (
             <img
               className="card-img-top"
               src={props.imagelink}
               alt="Card image cap"
+              style={{
+                height: "200px",
+                objectFit: "cover",
+                width: "100%",
+              }}
             />
           ) : (
-            <img className="card-img-top" src={dummy} alt="Card image cap" />
+            <img
+              className="card-img-top"
+              src={dummy}
+              alt="Card image cap"
+              style={{
+                height: "200px",
+                objectFit: "cover",
+                width: "100%",
+              }}
+            />
           )}
-          <a href="#!">
-            <div className="mask rgba-white-slight waves-effect waves-light"></div>
-          </a>
         </div>
 
-        <div className="card-body">
+        <div className="card-body" style={{ flex: "1 1 auto", paddingBottom: "20px" }}>
           <h4 className="card-title">{props.title}</h4>
           <p className="card-text">
             {props.person} <br /> {props.rollnumber}
@@ -46,10 +59,7 @@ function ProjectCards(props) {
               Manage
             </Link>
           ) : (
-            <a
-              href={props.morelink}
-              className="btn btn-primary waves-effect waves-light"
-            >
+            <a href={props.morelink} className="btn btn-primary waves-effect waves-light">
               Read more
             </a>
           )}
@@ -60,9 +70,8 @@ function ProjectCards(props) {
                 <span
                   style={{ marginLeft: "3px", cursor: "pointer" }}
                   className="fas fa-trash"
-                  onClick={() => handleDelete(props.id)} 
+                  onClick={() => handleDelete(props.id)}
                 ></span>
-                
               </div>
             ) : null}
           </div>
