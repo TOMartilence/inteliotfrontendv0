5187b3c037d0df7045225bd44c401386e608f183
Rahul <27rahulkofficial@gmail.com>
2025-01-05 14:50:23 +0530

    I did some changes in the notification panel in the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,29 @@
-import React, { useState } from "react";
-import { Row, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Row, Container, Col } from "react-bootstrap";
 import HomeCard from "../Components/HomeCard";
 import MovingCard from "../Components/MovingCard";
 import Homeeditbar from "../Components/Homeeditbar";
-import {Col} from "react-bootstrap"
-import Yt from "../Components/Yt"
-import Typer from "../Components/Typer"
-import { useEffect } from "react";
+import Yt from "../Components/Yt";
 import axios from "axios";
-import Loader from "../Components/Loader"
+import Loader from "../Components/Loader";
+import Typer from "../Components/Typer";
+
 function Home() {
   var loader = "";
   const [display, setDisplay] = useState("none");
   const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
   const [notif, setNotif] = useState([]);
-  const [video,setVideo] = useState([]);
-  
+  const [video, setVideo] = useState([]);
+
   const handlePopUp = async () => {
     try {
       setCount((prevCount) => prevCount + 1);
-      if (count >= 0 && count % 2 === 0) {
-        setDisplay("");
-      } else {
-        setDisplay("none");
-      }
+      setDisplay(count >= 0 && count % 2 === 0 ? "" : "none");
     } catch (error) {}
   };
-  const onload = async (e) => {
+
+  const onload = async () => {
     try {
       const response = await axios.get(`${process.env["REACT_APP_backendbaseurl"]}/api/`);
       setData(response.data.data);
@@ -36,96 +32,115 @@ function Home() {
       console.log(data);
     } catch (error) {}
   };
+
   useEffect(() => {
     onload();
   }, []);
+
   return (
-    <>
+    <div style={{ backgroundColor: "#0f0c29" }}>
       <div className="firstPage">
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center">
-            <Typer sentence="Technology is best when it brings people together. - Matt Mullenweg"/> <br />
-              <h3 className="animate-charcter" > INTEL I o T CLUB </h3> <br />
-              
+              <Typer sentence="Technology is best when it brings people together. - Matt Mullenweg" />
+              <br />
+              <h3 className="animate-charcter" style={{ fontSize: "2.5rem",
+               }}>
+                INTEL IoT CLUB      
+              </h3>
+              <br />
             </div>
           </div>
         </div>
       </div>
+
+      {/* Notification Panel moved to right and down */}
       <div
-        className="secondPage"
+        className="notificationPanel"
         style={{
-          backgroundImage:
-            "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
-            minHeight:"20vh"
+          position: "absolute",  // Changed back from fixed to absolute
+          top: "100px",
+          right: "20px",
+          width: "25%",
+          backgroundColor: "#ffffff",
+          padding: "15px",
+          borderRadius: "8px",
+          overflowY: "auto",
+          maxHeight: "87vh",
+          zIndex: "1000",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)"
         }}
       >
-        <Container>
-          <h3 style={{ textAlign: "center", marginBottom: "60px",marginTop:"30px" ,backgroundColor:"white",color:"black",padding:"5px"}}>
-            Notifications
-            <span style={{color:"red",fontSize:"25px",marginLeft:"20px"}} className="fas fa-bell"></span>
-          </h3>
-          <Homeeditbar display={display} />
-          {notif.length === 0 ? (
-            <>
-            <p style={{position:"relative"}}><Loader display={loader}/></p>
-            
-
-            </>
-          ) : (
-            notif.reverse().map((item, index) => (
-              <HomeCard
-                key={index}
-                id={item.id}
-                title={item.title}
-                description={item.description}
-                align={item.alignment}
-                bgcol={item.bgColor}
-                col={item.textColor}
-                date = {item.submissionDate}
-                time = {item.submissionTime}
-              />
-            ))
-          )}
-        </Container>
+        <h3
+          style={{
+            textAlign: "center",
+            marginBottom: "20px",
+            color: "black",
+            fontSize: "1.25rem",
+          }}
+        >
+          Notifications <span style={{ color: "red" }} className="fas fa-bell"></span>
+        </h3>
+        <Homeeditbar display={display} />
+        {notif.length === 0 ? (
+          <p style={{ position: "relative" }}>
+            <Loader display={loader} />
+          </p>
+        ) : (
+          notif.reverse().map((item, index) => (
+            <HomeCard
+              key={index}
+              id={item.id}
+              title={item.title}
+              description={item.description}
+              align={item.alignment}
+              bgcol={item.bgColor}
+              col={item.textColor}
+              date={item.submissionDate}
+              time={item.submissionTime}
+            />
+          ))
+        )}
       </div>
-      <div className="thirdPage" style={{
-        backgroundImage: "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
-        minHeight: "200vh"
-      }}>
+
+      {/* Main Content Area */}
+      <div style={{ padding: "20px" }}>
         <Container>
-        <Row>
-            <h3 style={{
+          <h3
+            style={{
               textAlign: "center",
               marginBottom: "60px",
               backgroundColor: "white",
               color: "black",
-              padding: "5px"
-            }}>
-              Upcoming Events <span className="fas fa-fire"></span>
-            </h3>
-
-            <Row>
-            <MovingCard data= {data}/>
-            </Row>
+              padding: "5px",
+            }}
+          >
+            Upcoming Events <span className="fas fa-fire"></span>
+          </h3>
+          <Row>
+            <MovingCard data={data} />
           </Row>
           <Row>
-            <h3 style={{
-              textAlign: "center",
-              marginBottom: "60px",
-              backgroundColor: "white",
-              color: "black",
-              padding: "5px"
-            }}>
+            <h3
+              style={{
+                textAlign: "center",
+                marginBottom: "60px",
+                backgroundColor: "white",
+                color: "black",
+                padding: "5px",
+              }}
+            >
               Trailers and Teasers <span className="fas fa-video"></span>
             </h3>
           </Row>
           <Row>
             {video.length === 0 ? (
-              <p style={{ position: "relative" }}><Loader display={loader} /></p>
+              <p style={{ position: "relative" }}>
+                <Loader display={loader} />
+              </p>
             ) : (
               video.map((item, index) => (
-                
                 <Col key={index} md={6} lg={4}>
                   <Yt link={item.link} />
                 </Col>
@@ -133,21 +148,8 @@ function Home() {
             )}
           </Row>
         </Container>
-        {sessionStorage.getItem("token") && (
-          <div style={{ position: "fixed", top: "650px", right: "30px" }}>
-            <button onClick={handlePopUp} className="addprojectbutton" style={{
-              height: "40px",
-              width: "40px",
-              borderRadius: "50%",
-              border: "none",
-              backgroundColor: "white",
-            }}>
-              <span className="fas fa-pen"></span>
-            </button>
-          </div>
-        )}
       </div>
-    </>
+    </div>
   );
 }
 
